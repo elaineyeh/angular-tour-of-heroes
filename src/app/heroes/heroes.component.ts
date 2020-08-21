164ec2bfeac6197e34ff3e5ca251c90695c2d7d1
elaineyeh <10456018@ntub.edu.tw>
2020-08-21 15:25:19 +0800

    feat: add app-routing and dashboard component to practice router

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,16 +14,10 @@ export class HeroesComponent implements OnInit {
 
   constructor(private heroService: HeroService, private messageService: MessageService) {}
 
-  selectedHero: Hero;
   heroes: Hero[];
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   ngOnInit(): void {
